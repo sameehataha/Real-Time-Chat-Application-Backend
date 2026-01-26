5322df8f59860333443efa721c09c68dbd3919aa
sameeha taha <sameeha.taha@icloud.com>
2026-01-27 02:15:07 +0530

    config changes

--- constants/config.js
+++ constants/config.js
@@ -1,11 +1,16 @@
 const corsOption = {
   origin: [
-    "https://real-time-chat-application.vercel.app",
+    "https://real-time-chat-application-frontend-h9xpu3ht9.vercel.app",
     "http://localhost:5173",
     "http://localhost:4173",
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:3001",
   ],
   credentials: true,
-  methods: ["GET", "POST"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 
